53579773e080408868382683682f2282bd6d4436
Steve Engledow <steve@offend.me.uk>
2025-01-10 12:10:19 +0000

    Add go mod

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-GOOS=linux go build -o shue-linux
-GOOS=darwin go build -o shue-osx
-GOOS=windows go build -o shue-windows.exe
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/stilvoid/shue
+
+go 1.23.4
+
+require github.com/lucasb-eyer/go-colorful v1.2.0
