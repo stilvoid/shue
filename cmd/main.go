db51a0982e06d848b65a26d062ff99f619429e2d
Steve Engledow <steve@offend.me.uk>
2025-01-22 00:10:45 +0000

    Add long help text

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/stilvoid/shue"
 
@@ -16,7 +15,7 @@ var lighten int
 var invert bool
 
 func init() {
-	rootCmd.Flags().StringVarP(&format, "format", "f", "all", fmt.Sprintf("Format to output: %s", strings.Join(shue.Formats, ", ")))
+	rootCmd.Flags().StringVarP(&format, "format", "f", "all", "Output format. See --help for details")
 	rootCmd.Flags().IntVarP(&lighten, "lighten", "l", 100, "Percentage by which to adjust the brightness")
 	rootCmd.Flags().BoolVarP(&invert, "invert", "i", false, "Invert the colour value - lighten is applied first")
 
@@ -26,7 +25,30 @@ func init() {
 var rootCmd = &cobra.Command{
 	Use:   "shue [COLOUR]",
 	Short: "Shue converts and modifies colour values for use in CSS etc.",
-	Args:  cobra.ExactArgs(1),
+	Long: `Shue converts and modifies CSS colour values.
+
+The following formats can be read and output by shue:
+
+  Name    Example
+  ---     ---
+  hex3    #123
+  hex4    #1234
+  hex6    #123456
+  hex8    #12345678
+  rgb     rgb(12, 34, 56)
+  rgba    rgba(12, 45, 56, 0.7)
+  hsl     hsl(180, 25%, 50%)
+  hsla    hsla(180, 25%, 50%, 0.75)
+
+If the input format contains an alpha value (hex4,hex8,rgba,hsla)
+but the output format does not (hex3,hex6,rgb,hsl)
+then shue will pre-multiply the colour before outputting.
+
+Shue can lighten a colour by specifying --lighten with a value 100+
+or darken a colour by specifying a value below 100.
+
+If you specify --lighten and --invert, inversion takes place _after_ lightening`,
+	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		input := args[0]
 
